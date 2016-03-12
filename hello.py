ec5f334de0fc9f81e286177e6939320269c059be
xz_li <xz_li@foxmail.com>
2016-03-12 16:45:04 +0800

    成功爬取图片
    使用正则匹配到所有的图片链接，然后用retrieve下载下来

--- hello.py
+++ hello.py
@@ -14,6 +14,30 @@ header = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko'
 } 
 
+def getImg(m):
+    print('In getImg')
+    imglist = re.findall(r'src="[\w\.\/]+jpg"', str(m))  
+    #imglist = re.findall(r'images/tb6.jpg', str(m))  
+    #imglist = re.findall(r'_mediafile/snutedu/2015/04/30/34csr9l1ii.jpg', str(m))  
+    imglist = list(set(imglist))
+
+    #imgurl = imglist.pop()
+    imglist = re.findall('[\w\.\/]+jpg', str(imglist))
+
+    temp = imglist.pop()
+    templist = 'http://www.snut.edu.cn/' + str(temp)
+
+    print(templist)
+
+    image = urllib.request.urlretrieve(templist, "/home/acer/python/webapp/test.jpg", None)
+
+    return imglist
+
+    #x = 0  
+    #for imgurl in imglist:  
+    #    urllib.urlretrieve(imgurl,'%s.jpg' % x)  
+    #    x = x + 1 
+
 def spider(url):
     #sName = 'test.html'
     #print('正在下载第' + str(i) + '个网页，并将其存储为' + sName + '......')
@@ -51,6 +75,7 @@ def spider(url):
             print('%s 不可达' % url)
         has_q.append(url)
         check = re.findall(r'http://[\w.]+snut.edu.cn[\w\/]+', str(m))
+        imglist = getImg(m)
         #list去重，先将list转为set再转为list即可
         check = list(set(check))
 
@@ -61,11 +86,13 @@ def spider(url):
                 wait_q.append(x)
 
         try:
-            url = wait_q.pop()
+            #url = wait_q.pop()
+            break
         except IndexError:
             print('Done.')
             f.close()
             break
+
 
             
         #while x:
